2574f84f8a604e7643b381b81fceed989f9affff
Murshed Alam Sifat <142529114+i-sifat@users.noreply.github.com>
2024-01-20 11:07:29 -0500

    Update install_flutter.sh

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 # Check if Flutter is already installed
-if [ -d "$HOME/flutter" ]; then
+if [ -d "$HOME/development/flutter" ]; then
   echo "Flutter is already installed."
   exit 0
 fi
 
 # Navigate to the home folder
-mkdir ~/development
-cd ~/development
+mkdir -p "$HOME/development"
+cd "$HOME/development"
 
 # Download and extract Flutter
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.4-stable.tar.xz
-tar xf flutter_linux_3.13.4-stable.tar.xz
+FLUTTER_VERSION="3.16.8"
+FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
+wget "$FLUTTER_URL"
+tar xf "flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
 # Add Flutter to your PATH in the .bashrc file
-echo 'export PATH="$PATH:$HOME/isifat/development/flutter/bin"' >> $HOME/.bashrc
-source $HOME/.bashrc
+echo 'export PATH="$PATH:$HOME/development/flutter/bin"' >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
 
 # Accept Android licenses
 flutter doctor --android-licenses
+flutter precache
+
 
 # Clean up the downloaded archive
-rm flutter_linux_3.13.3-stable.tar.xz
+rm "flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
 # Verify Flutter installation
 flutter doctor
 
 echo "Flutter has been successfully installed."
 exit 0
-
